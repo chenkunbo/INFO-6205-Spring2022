74f95be073bc33dda62f63f8de2056e6349e4ebb
chenkunbo <kikichen6677@gmail.com>
2022-01-30 20:25:00 -0800

    Committing assignment1

--- Assignment1/src/com/company/Main.java
+++ Assignment1/src/com/company/Main.java
@@ -141,7 +141,30 @@ public class Main {
         return result;
     }
 
+    // 658. Find K Closest Elements ( HW-5 )
+    private List<Integer> findClosestElements(int[] arr, int k, int x){
+        int left = 0;
+        int right = arr.length - k;
 
+        while(left < right){
+            int mid = (left +right)/2;
+            if(x - arr[mid] > arr[mid + k] - x){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = left; i < left + k; i++){
+            result.add(arr[i]);
+        }
+        return result;
+    }
 
+    // 767. Reorganize String ( HW-6 )
+    private string reorganizeString(string s){
+
+    }
 
 }
