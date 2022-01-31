684562c14b9a5f429223f563f9f33ec46002daa2
chenkunbo <kikichen6677@gmail.com>
2022-01-30 18:44:31 -0800

    Committing assignment1

--- Assignment1/src/com/company/Main.java
+++ Assignment1/src/com/company/Main.java
@@ -1,42 +1,95 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {2,0,2,1,1,0};
-        sortColors(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] nums1 = {2, 0, 2, 1, 1, 0};
+        sortColors(nums1);
+        System.out.println(Arrays.toString(nums1));
+        int[] nums2 = {3, 2, 3};
+        majorityElement(nums2);
+        System.out.println(Arrays.toString(nums2));
 
     }
 
-    private static void swap(int[] nums, int a, int b){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+    private static void swap(int[] nums1, int a, int b) {
+        int temp = nums1[a];
+        nums1[a] = nums1[b];
+        nums1[b] = temp;
     }
 
-    private static void sortColors(int[] nums){
+    // 75. Sort Colors ( HW-1 )
+    private static void sortColors(int[] nums1) {
         int point0 = 0;
-        int point2 = nums.length - 1;
+        int point2 = nums1.length - 1;
         int i = 0;
 
-        while(i <= point2){
-            if(nums[i] == 0){
-                swap(nums, point0, i);
+        while (i <= point2) {
+            if (nums1[i] == 0) {
+                swap(nums1, point0, i);
                 i++;
                 point0++;
-            }
-            else if(nums[i] == 1){
+            } else if (nums1[i] == 1) {
                 i++;
-            }
-            else{
-                swap(nums, point2, i);
+            } else {
+                swap(nums1, point2, i);
                 point2--;
             }
         }
     }
 
+    // 229. Majority Element II ( HW-2 )
 
-}
+        private static List<Integer> majorityElement(int[] nums2) {
+            List<Integer> res = new ArrayList<>();
+
+            if (nums2 == null || nums2.length == 0) {
+                return res;
+            }
+
+            int count1 = 0;
+            int count2 = 0;
+
+            int candidate1 = 1;
+            int candidate2 = 1;
+
+            for (int i = 0; i < nums2.length; i++) {
+
+                if (nums2[i] == candidate1) {
+                    count1++;
+                } else if (nums2[i] == candidate2) {
+                    count2++;
+                } else if (count1 == 0) {
+                    candidate1 = nums2[i];
+                    count1++;
+                } else if (count2 == 0) {
+                    candidate2 = nums2[i];
+                    count2++;
+                } else {
+                    count1--;
+                    count2--;
+                }
+            }
+
+
+            for (int n : nums2) {
+                if (n == candidate1) {
+                    count1++;
+                } else if (n == candidate2) {
+                    count2++;
+                }
+            }
+            if (count1 > nums2.length / 3) {
+                res.add(candidate1);
+            }
+            if (count2 > nums2.length / 3) {
+                res.add(candidate2);
+            }
+            return res;
+        }
+    }
+
