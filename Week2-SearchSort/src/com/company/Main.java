6ee9962d6557e1f58c18cfbbb26aa0c1c4ee287b
chenkunbo <kikichen6677@gmail.com>
2022-02-04 03:25:43 -0800

    Week-2 class : Searching

--- Week2-SearchSort/src/com/company/Main.java
+++ Week2-SearchSort/src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code
+        // write your code
     }
+
+    /// region Class 2
+
+    private static boolean binSearchIterative(int[] arr, int x){
+        if(arr == null || arr.length == 0){
+            return false;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int mid = (start + end)/2;
+            if(arr[mid] == x){
+                return true;
+            }
+            else if(arr[mid] < x){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+
+    /// endregion
+
 }
