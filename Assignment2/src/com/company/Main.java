d81336ec3b180488a6efeabf898e36fde84b5dbb
chenkunbo <kikichen6677@gmail.com>
2022-02-05 23:30:47 -0800

    Week-2 assignment

--- Assignment2/src/com/company/Main.java
+++ Assignment2/src/com/company/Main.java
@@ -1,19 +1,53 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //* 35. Search Insert Position ( HW-1 )
+        int[] nums1 = {1, 2, 3, 4, 5, 6};
+        int target1 = 6;
+        int[] nums2 = {1, 3, 5, 6};
+        int target2 = 2;
+        System.out.println("1. Search Insert Position");
+        System.out.println(searchInsearch(nums1, target1));
+        System.out.println(searchInsearch(nums2, target2));
+
+
+    }
+
+/// region code assignment-2
+
+    //* 35. Search Insert Position ( HW-1 )
+    private static int searchInsearch(int[] nums1, int target){
+        int l = 0;
+        int r = nums1.length - 1;
+
+        while(l <= r){
+            int midIndex = (l + r)/2;
+            if(nums1[midIndex] == target){
+                return midIndex;
+            }
+            else if(nums1[midIndex] > target){
+                r = midIndex - 1;
+            }
+            else{
+                l = midIndex + 1;
+            }
+        }
+        return l;
     }
 
 
 
-    // 35. Search Insert Position
-    private static
 
 
 
 
 
+
+/// endregion
 
 }
