d0b04158566112bb9f3dd03feae5bbdb7a18aa8a
chenkunbo <kikichen6677@gmail.com>
2022-02-06 01:05:32 -0800

    Week-2 assignment

--- Assignment2/src/com/company/Main.java
+++ Assignment2/src/com/company/Main.java
@@ -15,7 +15,12 @@ public class Main {
         System.out.println(searchInsearch(nums1, target1));
         System.out.println(searchInsearch(nums2, target2));
 
-
+        //* 540. Single Element in a Sorted Array ( HW-2 )
+        int[] nums3 = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+        int[] nums4 = {3, 3, 7, 7, 10, 11, 11};
+        System.out.println("2. Single Element in a Sorted Array");
+        System.out.println(singleNonDuplicate(nums3));
+        System.out.println(singleNonDuplicate(nums4));
     }
 
 /// region code assignment-2
@@ -40,8 +45,44 @@ public class Main {
         return l;
     }
 
+    //* 540. Single Element in a Sorted Array ( HW-2 )
+    private static int singleNonDuplicate(int[] nums3){
 
+        int start = 0;
+        int end = nums3.length - 1;
 
+        while(start <= end){
+            /*
+               We can also calculate mid = (start + end)/2. To prevent overflow
+               we are using start+(end-start)/2
+             */
+            int mid = start + ( end - start)/2;
+
+            if(start == end){
+                return nums3[start];
+            }
+            else if(mid % 2 == 0){
+
+                if(nums3[mid] == nums3[mid + 1]){
+                    start = mid + 2;
+                }
+                else{
+                    end = mid;
+                }
+            }
+            else{
+                if(nums3[mid] == nums3[mid - 1]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //* 153. Find Minimum in Rotated Sorted Array ( HW-3 )
 
 
 
