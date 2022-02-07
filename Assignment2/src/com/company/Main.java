c2aae91c73d83d0eaebde5d01173d0384aeb528b
chenkunbo <kikichen6677@gmail.com>
2022-02-06 18:01:03 -0800

    Week-2 assignment

--- Assignment2/src/com/company/Main.java
+++ Assignment2/src/com/company/Main.java
@@ -219,7 +219,30 @@ public class Main {
         return res;
     }
 
+    //* 57. Insert Interval ( HW-7 )
+    private static int[][] insert(int[][] intervals, int[] newInterval){
+        // define list
+        List<int[]> list = new LinkedList<>();
 
+        // 1. add all intervals that are non-overlapping to the newInterval
+        int i = 0;
+        int n = intervals.length;
+        while(i < n && intervals[i][1] < newInterval[0]){
+            list.add(intervals[i++]);
+        }
+        // 2. merge newinterval with the overlapping interval
+        while(i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i ++;
+        }
+        list.add(newInterval);
+        // 3. add the remaining intervals onto the list
+        while(i < n){
+            list.add(intervals[i++]);
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
 
 
 
