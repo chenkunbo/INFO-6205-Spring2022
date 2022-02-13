4608cd6b57b03affb39c84961cedb361cf7fa96d
chenkunbo <kikichen6677@gmail.com>
2022-02-12 23:19:08 -0800

    Week3-Linked List

---
+++ Assignment3/src/com/company/ListNode.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int cal, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
