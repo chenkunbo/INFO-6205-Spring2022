d6d11d97df6c4e9c51cfca5264679b908ac1c9c9
chenkunbo <kikichen6677@gmail.com>
2022-02-14 20:53:47 -0800

    Week3-Linked List

--- Assignment3/src/com/company/Main.java
+++ Assignment3/src/com/company/Main.java
@@ -10,32 +10,43 @@ public class Main {
     }
 
     // 2. Add Two Numbers
-    private ListNode addTwoNumbers(ListNode l1, ListNode l2){
-        ListNode dummy = new ListNode(0);
-        ListNode(p = l1, q = l2, curr = dummy);
-        int carry = 0;
-
-        while(p != null || q != null){
-            int x = p != null ? p.val : 0;
-            int y = q != null ? q.val : 0;
-            int sum = x + y + carry;
-            carry = sum/10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if(p != null) p = p.next;
-            if(q != null) q = q.next;
-
-        }
-        if(carry > 0){
-            curr.next = new ListNode(carry);
-        }
-        return dummy.next;
-
+    /* Definition for singly-linked list.
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){ val = x; }
     }
-
-
-
-
-
+     */
+    private ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        int v1 = 0;
+        int v2 = 0;
+        int sum = 0;
+        int carry = 0;
+        ListNode sumHead = new ListNode(-1);
+        ListNode ptr = sumHead;
+        while(l1 != null || l2 != null){
+            if(l1 != null){
+                v1 = l1.val;
+                l1 = l1.next;
+            }
+            else v1 = 0;
+            if(l2 != null){
+                v2 = l2.val;
+                l2 = l2.next;
+            }
+            else v2 = 0;
+            sum = v1 + v2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            ListNode temp = new ListNode(sum);
+            ptr.next = temp;
+            ptr = ptr.next;
+        }
+        if(carry != 0){
+            ListNode temp = new ListNode(carry);
+            ptr.next = temp;
+        }
+        return sumHead.next;
+    }
 
 }
