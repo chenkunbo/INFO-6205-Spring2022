160e45a09af06f5a05bc814bb3983a16b0d768ec
chenkunbo <kikichen6677@gmail.com>
2022-02-16 00:59:32 -0800

    Week3-Linked List

---
+++ Assignment3/src/HW7_Odd_Even_Linked_List_328/ListNode.java
@@ -0,0 +1,14 @@
+package HW7_Odd_Even_Linked_List_328;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
