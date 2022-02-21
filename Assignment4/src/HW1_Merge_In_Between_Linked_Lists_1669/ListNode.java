74a3d663f7e6786b7251bbe52183077350757ce3
chenkunbo <kikichen6677@gmail.com>
2022-02-20 21:45:40 -0800

    Week4-Linked List

---
+++ Assignment4/src/HW1_Merge_In_Between_Linked_Lists_1669/ListNode.java
@@ -0,0 +1,12 @@
+package HW1_Merge_In_Between_Linked_Lists_1669;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val = val;}
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
