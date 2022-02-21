9617d4383d4acdb2213273b0bc04b27610e8ae98
chenkunbo <kikichen6677@gmail.com>
2022-02-21 00:40:45 -0800

    Week4-Linked List

--- Assignment4/src/HW6_Linked_List_Random_Node_382/Solution.java
+++ Assignment4/src/HW6_Linked_List_Random_Node_382/Solution.java
@@ -2,4 +2,27 @@ package HW6_Linked_List_Random_Node_382;
 
 public class Solution {
 
+    ListNode head = null;
+
+    // Note that the head is guaranteed to be not null, so it contains at least one node.
+
+    public Solution(ListNode head){
+        this.head = head;
+    }
+
+    // return a random node's value
+    public int getRandom(){
+        ListNode curr = head;
+        int i = 1;
+        int res = 0;
+        while(curr != null){
+            if(Math.random() < 1.0 / i){
+                // take this value
+                res = curr.val;
+            }
+            i ++;
+            curr = curr.next;
+        }
+        return res;
+    }
 }
