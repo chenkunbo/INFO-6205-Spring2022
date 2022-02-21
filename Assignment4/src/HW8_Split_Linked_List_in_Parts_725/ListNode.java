0d58273b993a149babfb23e3b4712c59e1d16e25
chenkunbo <kikichen6677@gmail.com>
2022-02-21 01:08:36 -0800

    Week4-Linked List

--- Assignment4/src/HW8_Split_Linked_List_in_Parts_725/ListNode.java
+++ Assignment4/src/HW8_Split_Linked_List_in_Parts_725/ListNode.java
@@ -1,4 +1,12 @@
 package HW8_Split_Linked_List_in_Parts_725;
 
 public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val = val;}
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
 }
--- Assignment4/src/HW8_Split_Linked_List_in_Parts_725/Solution.java
+++ Assignment4/src/HW8_Split_Linked_List_in_Parts_725/Solution.java
@@ -1,4 +1,36 @@
 package HW8_Split_Linked_List_in_Parts_725;
 
 public class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k){
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        int n = 0;
+        while(head != null){
+            head = head.next;
+            n ++;
+        }
+        int len = n / k;
+        int nod = n % k;
+        ListNode[] ans = new ListNode[k];
+        ListNode pre = dummy;
+        head = dummy.next;
+        int i = 0;
+        while(i < k){
+            ans[i] = head;
+            for(int j = 0; j < len; j++){
+                pre = head;
+                head = head.next;
+            }
+            if(i < nod){
+                pre = head;
+                head = head.next;
+            }
+            pre.next = null;
+            i++;
+        }
+        return ans;
+    }
 }
+
+
+
