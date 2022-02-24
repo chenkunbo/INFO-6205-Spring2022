0b56fab5d810cc4b480209fec79fdded40bd353a
chenkunbo <kikichen6677@gmail.com>
2022-02-24 03:20:46 -0800

    Week5-Binary Tree

---
+++ Assignment5/src/HW1_Binary_Tree_Level_Order_Traversal_II_107/Solution.java
@@ -0,0 +1,43 @@
+package HW1_Binary_Tree_Level_Order_Traversal_II_107;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null)
+            return result;
+
+        Stack<List<Integer>> st = new Stack<>();
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(root);
+
+        while(!qu.isEmpty()){
+            int len = qu.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0; i < len; i ++) {
+                TreeNode temp = qu.poll();
+                level.add(temp.val);
+
+                if (temp.left != null) {
+                    qu.add(temp.left);
+                }
+
+                if (temp.right != null) {
+                    qu.add(temp.right);
+                }
+            }
+            st.push(level);
+
+        }
+        while(!st.isEmpty()){
+            result.add(st.pop());
+        }
+        return result;
+
+    }
+
+
+}
---
+++ Assignment5/src/HW1_Binary_Tree_Level_Order_Traversal_II_107/TreeNode.java
@@ -0,0 +1,14 @@
+package HW1_Binary_Tree_Level_Order_Traversal_II_107;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
