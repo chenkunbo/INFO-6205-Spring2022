95b7ef6fc11b949f2d7cf97a7309a1ff979acc66
chenkunbo <kikichen6677@gmail.com>
2022-02-24 20:56:16 -0800

    Week5-Binary Tree

---
+++ Assignment5/src/HW03_Maximum_Width_of_Binary_Tree_662/Solution.java
@@ -0,0 +1,11 @@
+package HW03_Maximum_Width_of_Binary_Tree_662;
+
+import java.util.HashMap;
+
+public class Solution {
+    int max_width;
+    HashMap<Integer,Integer> leftmost_positions;
+
+
+
+}
---
+++ Assignment5/src/HW03_Maximum_Width_of_Binary_Tree_662/TreeNode.java
@@ -0,0 +1,15 @@
+package HW03_Maximum_Width_of_Binary_Tree_662;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
