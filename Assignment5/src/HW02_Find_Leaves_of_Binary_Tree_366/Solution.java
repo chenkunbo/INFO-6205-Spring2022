939c98745af3107f77b89aaff45e1644912c3677
chenkunbo <kikichen6677@gmail.com>
2022-02-24 20:46:20 -0800

    Week5-Binary Tree

--- Assignment5/src/HW02_Find_Leaves_of_Binary_Tree_366/Solution.java
+++ Assignment5/src/HW02_Find_Leaves_of_Binary_Tree_366/Solution.java
@@ -1,4 +1,29 @@
 package HW02_Find_Leaves_of_Binary_Tree_366;
 
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root){
+
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        level(root, ans);
+        return ans;
+
+    }
+    public Integer level(TreeNode root, List<List<Integer>> ans){
+        if(root == null) return -1;
+        int level = Math.max(level(root.left, ans), level(root.right, ans)) + 1;
+        if(ans.size() <= level){
+            ans.add(new ArrayList<>());
+        }
+
+        ans.get(level).add(root.val);
+        return level;
+    }
+
+
 }
--- Assignment5/src/HW02_Find_Leaves_of_Binary_Tree_366/TreeNode.java
+++ Assignment5/src/HW02_Find_Leaves_of_Binary_Tree_366/TreeNode.java
@@ -1,4 +1,16 @@
 package HW02_Find_Leaves_of_Binary_Tree_366;
 
+import com.sun.source.tree.Tree;
+
 public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
