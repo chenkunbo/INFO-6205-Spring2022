e96a97e919cfbb06cd4c723dff586c99574c4f90
chenkunbo <kikichen6677@gmail.com>
2022-03-07 19:51:26 -0800

    Week6-Binary Tree

---
+++ Assignment6/src/HW02_Lowest_Common_Ancestor_of_a_Binary_Tree_236/Solution.java
@@ -0,0 +1,19 @@
+package HW02_Lowest_Common_Ancestor_of_a_Binary_Tree_236;
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        //Base case
+        if(root == null) return null;
+        if(root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        //We found p and q in left and right
+        if(left != null && right != null) return root;
+
+        if(left != null) return left;
+
+        return right;
+    }
+}
---
+++ Assignment6/src/HW02_Lowest_Common_Ancestor_of_a_Binary_Tree_236/TreeNode.java
@@ -0,0 +1,10 @@
+package HW02_Lowest_Common_Ancestor_of_a_Binary_Tree_236;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
