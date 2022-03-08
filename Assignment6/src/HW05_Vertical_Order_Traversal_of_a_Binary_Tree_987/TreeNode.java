de1059965d79a6af6b0d7acb6291efa9c2201214
chenkunbo <kikichen6677@gmail.com>
2022-03-07 21:30:49 -0800

    Week6-Binary Tree

---
+++ Assignment6/src/HW05_Vertical_Order_Traversal_of_a_Binary_Tree_987/TreeNode.java
@@ -0,0 +1,16 @@
+package HW05_Vertical_Order_Traversal_of_a_Binary_Tree_987;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
