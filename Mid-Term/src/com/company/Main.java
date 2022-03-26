3db8bb4d6b85f17f9e2bd7b36d8602eba654739d
chenkunbo <kikichen6677@gmail.com>
2022-03-26 11:23:27 -0700

    Mid Term Code

--- Mid-Term/src/com/company/Main.java
+++ Mid-Term/src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -20,6 +18,8 @@ public class Main {
         //System.out.println(addTwoNumbers(l1, l2));
 
         //Question 3:
+        int[] preorder = {};
+        int[] inorder = {};
 
 
 
@@ -91,6 +91,33 @@ public class Main {
 
 
     //Question 3:
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        int preorderIndex;
+        Map<Integer, Integer> inorderIndexMap;
+        public TreeNode buildTree (int[] preorder, int[] inorder){
+            preorderIndex = 0;
+            // build a hashmap to store value -> its index relations
+            inorderIndexMap = new HashMap<>();
+            for (int i = 0; i < inorder.length; i++) {
+                inorderIndexMap.put(inorder[i], i);
+            }
+
+            return arrayToTree(preorder, 0, preorder.length - 1);
+        }
+
+        private TreeNode arrayToTree ( int[] preorder, int left, int right){
+            if (left > right) return null;
+
+            int rootValue = preorder[preorderIndex++];
+            TreeNode root = new TreeNode(rootValue);
+
+            root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
+            root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
+
+            return root;
+        }
+    }
 
 
 
