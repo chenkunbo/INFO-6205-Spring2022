3c43d23ef1fa5b336bb3803e6f55aa6154fda4a2
chenkunbo <kikichen6677@gmail.com>
2022-03-26 11:14:25 -0700

    Mid Term Code

--- Mid-Term/src/com/company/Main.java
+++ Mid-Term/src/com/company/Main.java
@@ -57,38 +57,31 @@ public class Main {
     int carry = 0;
     while (l1 != null || l2 != null) {
         int sum = carry;
-                // Since the lengths of the lists may be unequal, we are checking if the
-                // current node is null for one of the lists
-                if (l1 != null) {
-                    sum += l1.val;
-                    l1 = l1.next;
-                }
-                if (l2 != null) {
-                    sum += l2.val;
-                    l2 = l2.next;
-                }
-                // At this point, we will add the total sum % 10 to the new node
-                // in the resultant list
-                ListNode node = new ListNode(sum % 10);
-                // Carry to be added in the next iteration
-                carry = sum / 10;
-                // If this is the first node or head
-                if (temp == null) {
-                    temp = head = node;
-                }
-                // For any other node
-                else {
-                    temp.next = node;
-                    temp = temp.next;
-                }
-            }
-            // After the last iteration, we will check if there is carry left
-            // If it's left then we will create a new node and add it
-            if (carry > 0) {
-                temp.next = new ListNode(carry);
-            }
-            return head;
+
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
         }
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        ListNode node = new ListNode(sum % 10);
+        carry = sum / 10;
+        if (temp == null) {
+            temp = head = node;
+        }else{
+            temp.next = node;
+            temp = temp.next;
+        }
+    }
+
+    if (carry > 0) {
+        temp.next = new ListNode(carry);
+    }
+    return head;
+    }
 
 
 
