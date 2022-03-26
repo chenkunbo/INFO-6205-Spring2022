1ca295bd019878634d9d24735fc95dda5d500663
chenkunbo <kikichen6677@gmail.com>
2022-03-26 13:15:39 -0700

    Mid Term Code

--- Mid-Term/src/Question_4/Solution.java
+++ Mid-Term/src/Question_4/Solution.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 public class Solution {
 
     public int[][] merge(int[][] intervals) {
+
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         LinkedList<int[]> merged = new LinkedList<>();
         for (int[] interval : intervals) {
@@ -20,4 +21,5 @@ public class Solution {
         }
         return merged.toArray(new int[merged.size()][]);
     }
+
 }
