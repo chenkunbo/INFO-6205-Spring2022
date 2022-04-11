aa7a355717a47c1e20b8448fd5db187efa1c89de
chenkunbo <kikichen6677@gmail.com>
2022-04-10 23:52:30 -0700

    Week8-Graph

---
+++ Assignment7/src/HW02_Shortest_Path_Visiting_All_Nodes_847/Solution.java
@@ -0,0 +1,46 @@
+package HW02_Shortest_Path_Visiting_All_Nodes_847;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    class Pair {
+        int i;
+        int path;
+        public Pair(int i, int path) {
+            this.i = i;
+            this.path = path;
+        }
+    }
+    public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        int allVisited = (1 << n) - 1;
+
+        boolean[][] visited = new boolean[n][1 << n];
+        Queue<Pair> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (1 << i == allVisited) return 0;
+            visited[i][1 << i] = true;
+            q.offer(new Pair(i, 1 << i));
+        }
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Pair p = q.poll();
+                int[] edges = graph[p.i];
+
+                for(int t: edges) {
+                    int path = p.path | (1 << t);
+                    if (path == allVisited) return step + 1;
+                    if (!visited[t][path]) {
+                        visited[t][path] = true;
+                        q.offer(new Pair(t, path));
+                    }
+                }
+            }
+            step++;
+        }
+        return step;
+    }
+}
