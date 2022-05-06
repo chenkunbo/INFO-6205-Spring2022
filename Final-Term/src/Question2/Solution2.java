fc212ea3e03b27b9f01ed5492dc25271312c28c1
chenkunbo <kikichen6677@gmail.com>
2022-05-06 10:18:31 -0700

    Final-Term Questions

--- Final-Term/src/Question2/Solution2.java
+++ Final-Term/src/Question2/Solution2.java
@@ -2,4 +2,10 @@ package Question2;
 
 
 public class Solution2 {
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, 1, -1, 0};
+    public static int numIslands(int[] nums) {
+
+    }
 }
+
